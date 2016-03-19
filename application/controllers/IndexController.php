4e2299234801ac2f10fe98c89ea63d5225f76b9c
Goxeta <d.fopuhag@gmail.com>
2016-03-19 21:25:01 +0100

    First version email receiver

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,18 +1,30 @@
 <?php
 
-class IndexController extends Zend_Controller_Action
-{
+class IndexController extends Zend_Controller_Action {
 
-    public function init()
-    {
+    public function init() {
         /* Initialize action controller here */
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         // action body
+        $storage = new Zend_Mail_Storage_Imap(array('host' => 'stud.prz.edu.pl',
+            'user' => '127274@stud.prz.edu.pl',
+            'password' => '16121992',
+            'ssl' => 'SSL'));
+        $i=1;
+        foreach ($storage as $message) {
+//		$message=$storage->getMessage(1);
+            echo '----------------------<br />' . "\n";
+            echo "From: " . $message->from . "<br />\n";
+            echo "To: " . $message->to . "<br />\n";
+            echo "Time: " . date("Y-m-d H:s", strtotime($message->date)) . "<br />\n";
+            echo "Subject: " . $message->subject . "<br />\n";
+            echo "<br />";
+            echo utf8_decode($message->getContent());
+            if($i==10) break;
+            $i++;
+        }
     }
 
-
 }
-
